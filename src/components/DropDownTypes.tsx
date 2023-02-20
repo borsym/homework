5af7fa90eea08ecba8d4706fc9c7ac9e11ce1c02
borsym <borsymatee@gmail.com>
2023-02-20 10:18:12 +0100

    dropdown init

---
+++ src/components/DropDownTypes.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface Props {
+  label: string;
+  optionsEndpoint: string;
+  value?: string;
+  onChange: (value: string) => void;
+}
+
+function DropDownTypes({ label, optionsEndpoint, value, onChange }: Props) {
+  return (
+    <div>
+      <label htmlFor={label} className="text-lg font-medium text-gray-700">
+        {label}:
+      </label>
+
+      <select
+        id={label}
+        name={label}
+        className="focus:ring-red-500 focus:border-red-500 block w-full pl-3 pr-12 sm:text-sm border-gray-300 rounded-md"
+        value={value}
+        // onChange={handleChange}
+      >
+        <option value="">Select an option</option>
+        {/* {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))} */}
+      </select>
+    </div>
+  );
+}
+
+export default DropDownTypes;
